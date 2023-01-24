32f002a18437523b286b46df0f22c6a87ffce571
Zoltan Aldott <zoltan.aldott@interworks.co.uk>
2023-01-24 15:56:26 +0000

    Adding function to open correct download link

--- Version_matching.py
+++ Version_matching.py
@@ -5,7 +5,9 @@ import re
 from zipfile import ZipFile
 import subprocess
 import ctypes
-
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import webbrowser
 
 def extract_version(versionline):
     """Extract the version number from the third line of the file"""
@@ -19,7 +21,6 @@ def extract_twbx(file_path):
     """Extract the twbx file and return the third line"""
     os.mkdir("tmp")
     try:
-        print(file_path)
         m=re.match(r".*\\([^\\]+.twb)x$",file_path)
         file_name=m.group(1)
         with ZipFile(file_path,"r") as twbx:
@@ -55,13 +56,30 @@ def check_version_match(version, file_path):
             if choice == 'open':
                 open_file_with_tableau(nearest_version, file_path)
             elif choice == 'download':
-                #download_version()
-                pass
-        else:
-            #download_version()
-            pass
+                download_version(version)
 
-
+def download_version(version):
+    req = Request("https://www.tableau.com/support/releases")
+    html_page = urlopen(req)
+    soup = BeautifulSoup(html_page, "lxml")
+    version_links = []
+    for link in soup.findAll('a'):
+        curr_link=str(link.get('href'))
+        if curr_link.find(version)!=-1:
+            version_links.append(curr_link)
+    version_dict={}
+    available_sub_versions=list()
+    for link in version_links:
+        m=re.match(".*/([\d\.]+)$",link)
+        sub_version=m.group(1)
+        if version==sub_version:
+            sub_version+=".0"
+        available_sub_versions.append(int(sub_version[7:]))
+        version_dict[sub_version]=link
+    if len(version_links)==0:
+        ctypes.windll.user32.MessageBoxW(0, f"This version is not supported by Tableau, please find a compatible version manually.", "Version too old", 1)
+    webbrowser.open(version_dict[version+"."+str(max(available_sub_versions))])
+    
 def get_installed_versions():
     """Get the list of installed versions of Tableau"""
     versions = []
@@ -82,12 +100,13 @@ def get_nearest_version(version, versions):
 def prompt_user(version, nearest_version):
     """Prompt the user to choose between opening the file with the nearest version or downloading the appropriate version"""
     messageBox = ctypes.windll.user32.MessageBoxW
-    result = messageBox(0, f"Tableau version {version} not found, the earliest version found is {nearest_version}. Do you want to open the file with this version?", "Version not found", 3)
+    result = messageBox(0, f"Tableau version {version} not found, the earliest version found is {nearest_version}. Do you want to open the file with this version? Pressing 'No' will open the download page for the appropriate version. ", "Version not found", 3)
     if result == 6:
         return 'open'
-    else:
-        messageBox(0, "Please download the appropriate version from tableau.com/esdalt", "Download the appropriate version", 1)
+    elif result == 7:
         return 'download'
+    else:
+        exit()
 
 def open_file_with_tableau(version, file_path):
     """Open the file with the matching version of Tableau"""
