936b25e27546455d2e608fdd16681a477e8d7130
Zoltan Aldott <zoltan.aldott@interworks.co.uk>
2023-01-24 10:27:08 +0000

    Refactored with ChatGPT

--- Tableau_matching.bat
+++ Tableau_matching.bat
@@ -1,2 +1,2 @@
+@echo off
 python "Version_matching.py" %*
-PAUSE
--- Version_matching.py
+++ Version_matching.py
@@ -7,40 +7,65 @@ import subprocess
 import ctypes  # An included library with Python install.   
 
 
-if sys.argv[1].endswith("twb"):
-    versionline=linecache.getline(sys.argv[1],3)
+def extract_version(versionline):
+    """Extract the version number from the third line of the file"""
+    
     m=re.match("[^a-zA-Z]*build (\d+)\..*",versionline)
     version=m.group(1)
     version=version[:4]+"."+version[-1]
+    return version
 
-elif sys.argv[1].endswith("twbx"):
+def extract_twbx(file_path):
+    """Extract the twbx file and return the third line"""
     os.mkdir("tmp")
-    with ZipFile(sys.argv[1],"r") as twbx:
-        twbx.extract(sys.argv[1][:-1],".\\tmp")
-    versionline=linecache.getline(f"tmp\\{sys.argv[1][:-1]}",3)
-    m=re.match("[^a-zA-Z]*build (\d+)\..*",versionline)
-    version=m.group(1)
-    version=version[:4]+"."+version[-1]
-    print(version)
-    os.remove(f"tmp\\{sys.argv[1][:-1]}")
+    with ZipFile(file_path,"r") as twbx:
+        twbx.extract(file_path[:-1],".\\tmp")
+    versionline=linecache.getline(f"tmp\\{file_path[:-1]}",3)
+    os.remove(f"tmp\\{file_path[:-1]}")
     os.rmdir("tmp")
-else:
-    raise NotImplementedError
-if "Tableau" not in os.listdir("C:\\Program Files"):
-    print("Tableau is not installed")
-    exit()
-versions=list()
-for file in os.listdir("C:\\Program Files\\Tableau"):
-    if "Tableau" in file and not "Prep" in file:
-        m=re.match("Tableau (\d+\.\d)",file)
-        versions.append(m.group(1))
-if version in versions:
-    print("Version match found")
+    return versionline
+
+def check_tableau_install():
+    """Check if Tableau is installed on the computer"""
+    if "Tableau" not in os.listdir("C:\\Program Files"):
+        print("Tableau is not installed")
+        exit()
+    return True
+
+def check_version_match(version):
+    """Check if the extracted version number is present in the Tableau folder"""
+    versions=list()
+    for file in os.listdir("C:\\Program Files\\Tableau"):
+        if "Tableau" in file and not "Prep" in file:
+            m=re.match("Tableau (\d+\.\d)",file)
+            versions.append(m.group(1))
+    if version in versions:
+        print("Version match found")
+        return True
+    else:
+        print("Version not found, please download appropriate version from tableau.com/esdalt")
+        ctypes.windll.user32.MessageBoxW(0, "Version not found", f"Tableau version not found: {version}. Download from tableau.com/esdalt", 1)
+        return False
+
+def open_file_with_tableau(version, file_path):
+    """Open the file with the matching version of Tableau"""
     DETACHED_PROCESS=0x00000008
     cwd=os.getcwd()
-    print(f'"{cwd}\{sys.argv[1]}"')
     os.chdir(f"C:\\Program Files\\Tableau\\Tableau {version}\\bin")
-    pid=subprocess.Popen(f'tableau.exe "{cwd}\{sys.argv[1]}"',creationflags=DETACHED_PROCESS)
-else:
-    print("Version not found, please download appropriate version from tableau.com/esdalt")
-    ctypes.windll.user32.MessageBoxW(0, "Version not found", f"Tableau version not found: {version}. Download from tableau.com/esdalt", 1)
+    pid=subprocess.Popen(f'tableau.exe "{cwd}\{file_path}"',creationflags=DETACHED_PROCESS)
+
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+    if file_path.endswith("twb"):
+        versionline=linecache.getline(file_path,3)
+        version = extract_version(versionline)
+
+    elif file_path.endswith("twbx"):
+        versionline = extract_twbx(file_path)
+        version = extract_version(versionline)
+    else:
+        raise NotImplementedError
+
+    if check_tableau_install():
+        if check_version_match(version):
+            open_file_with_tableau(version, file_path)
